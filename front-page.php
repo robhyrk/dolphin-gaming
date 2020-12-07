46e08807ee30663639b3ce2c892bcfe09ee1abd5
robhyrk <rob.hyrkiel@gmail.com>
2020-12-07 06:26:10 -0500

    styles: home page tables

--- front-page.php
+++ front-page.php
@@ -22,12 +22,68 @@
 //The WordPress Loop: loads post content 
     while( have_posts() ) :
         the_post(); ?>
-    <article class="container py-5">
+    <article id="explore-gaming" class="container py-5">
         <h2 class="px-4"><?php the_title(); ?></h2>
-        <p class=""><?php the_content(); ?></p>
+        <?php the_content(); ?>
     </article>
 
-    
+    <article id="bingo" class="container py-5">
+        <h2>BINGO AT DOLPHIN</h2>
+        <p>
+            Bingo is now better than ever! Come in and see how everyone's favourite childhood game 
+            has become the go-to place for fun and entertainment. Multiple ways to win and exciting 
+            jackpot prizes 7 days of the week. Bingo is a game of pure chance and anyone can play. 
+            Did you know that Bingo is a also charitable gaming? Therefore, part of the proceeds 
+            go back to local communities!
+        </p>
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary">Learn More</button>
+            <button type="button" class="btn btn-secondary">Jackpots</button>
+        </div>
+    </article>
+
+    <article id="taptix" class="container py-5">
+        <h2>Dolphin’s Taptix Machines</h2>
+        <p>
+        Scarborough has game! Our Taptix room is the place to be! Our Taptix machines provide 
+        the ultimate entertainment any day of the week from 8 am to 4 am. 
+        They are fun and simple to play.
+        </p>
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary">Learn More</button>
+            <button type="button" class="btn btn-secondary">Jackpots</button>
+        </div>
+    </article>
+
+    <article id="jackpots-home" class="container py-5">
+        <h2>Current Session:</h2>
+        <h2>Wed September 15</h2>
+        <h2 class="jackpot-time">
+            <span class="time-box">1</span><span class="time-box">0</span><span class="time-box">0</span><span class="time-box">0</span>
+            <span class="time-period">AM</span>
+        </h2>
+        <div class="taptix-jackpots">
+            <h2>Taptix Jackpots</h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Game Name</th>
+                        <th scope="col">Progressive Jackpot</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">IGT</th>
+                    <td>$36,000 EST</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Bathtime Bucks</th>
+                    <td>$5,000 EST</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </article>
     <!-- Loop ends -->
     <?php endwhile;?>
 
--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('browser-sync', function() {
   ];
 
   browserSync.init(files, {
-    proxy: 'localhost:8888/starter'
+    proxy: 'localhost:8888/dolphin-gaming'
   });
 
   gulp.watch(files).on('change', browserSync.stream);
